f3f7d436bc613c2c6726df36b2088c53f37d9e02
Daniel Whitaker <danielwh@microsoft.com>
2018-11-09 10:26:10 -0800

    Initial commit

--- UnsignTool/Program.cs
+++ UnsignTool/Program.cs
@@ -4,18 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnsignTool
+namespace Unsigntool
 {
     class Program
     {
+        [System.Runtime.InteropServices.DllImport("Imagehlp.dll ")]
+        private static extern bool ImageRemoveCertificate(IntPtr handle, int index);
+
+        private static void UnsignFile(string file)
+        {
+            using (System.IO.FileStream fs = new System.IO.FileStream(file, System.IO.FileMode.Open, System.IO.FileAccess.ReadWrite))
+            {
+                ImageRemoveCertificate(fs.SafeFileHandle.DangerousGetHandle(), 0);
+                fs.Close();
+            }
+        }
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            foreach (var arg in args)
+            {
+                UnsignFile(arg);
+            }
         }
     }
 }
